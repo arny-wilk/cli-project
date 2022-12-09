72a4063d9a347de00dacb395f0d6802cb7abacaf
arny-wilk <arnywilk@gmail.com>
2022-12-09 21:46:03 +0100

    implement UserService and UserDAO

--- src/users/UserDAO.java
+++ src/users/UserDAO.java
@@ -4,14 +4,26 @@ import enums.Enum;
 
 public class UserDAO {
     private static final User[] users;
+    private int sizeArray = users.length;
 
     static {
         users = new User[] {
-                new User("James", "Fernandez", Enum.GENDER.MALE, Enum.ADDRESS_TYPE.HOME, "7th CapTown Street", Enum.COUNTRY.ENGLAND, "jamesfernandez@gmail.com"),
+                new User("James"
+                        , "Fernandez"
+                        , Enum.GENDER.MALE
+                        , Enum.ADDRESS_TYPE.HOME
+                        , "7th CapTown Street"
+                        , Enum.COUNTRY.ENGLAND, "jamesfernandez@gmail.com"),
         };
     }
 
     public static User[] getUsers() {
         return users;
+    }
+
+    public void addUser(User[] users, User newUser) {
+        if(UserService.emailValidator(newUser)) {
+            users[sizeArray++] = newUser;
+        }
     }
 }
--- src/users/UserService.java
+++ src/users/UserService.java
@@ -1,4 +1,13 @@
 package users;
 
 public class UserService {
+    private User user;
+
+    static boolean emailValidator(User user) {
+        String email = user.getEmail();
+        if (!email.contains("@")) {
+            return false;
+        }
+        return true;
+    }
 }
