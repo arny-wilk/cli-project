c66f24518fd3dd2c1cf03910541a60b07fbb90b6
arny-wilk <arnywilk@gmail.com>
2023-02-04 15:03:30 +0100

    implemented interface

--- src/bookings/Book.java
+++ src/bookings/Book.java
@@ -1,47 +1,10 @@
 package bookings;
 
-import cars.Car;
-import cars.CarDAO;
-import users.User;
-import users.UserDAO;
-
-import java.time.LocalDate;
 import java.util.HashMap;
+import cars.Car;
+import users.User;
 
-public class Book {
-    private static final LocalDate dateBooking;
-    private static HashMap<Car, User> booking;
-
-    static {
-        dateBooking = LocalDate.now();
-        booking = new HashMap<>();
-        Car[] carInit = CarDAO.getCars();
-        User[] userInit = UserDAO.getUsers();
-        booking.put(carInit[0], userInit[0]);
-        // booking.put(carInit[1], userInit[1]);
-    }
-
-    public Book() {
-    }
-
-    public LocalDate getDateBooking() {
-        return dateBooking;
-    }
-
-    public HashMap<Car, User> getBooking() {
-        return booking;
-    }
-
-    public void setBooking(HashMap<Car, User> booking) {
-        Book.booking = booking;
-    }
-
-    void put(Car car, User user) {
-        booking.put(car, user);
-    }
-
-    @Override
-    public String toString() {
-        return "Book{" + "dateBooking=" + dateBooking + ", booking=" + booking + '}';
-    }
+public interface Book { 
+    HashMap<Car, User> getBooking();
+    void put(Car car, User user);
 }
--- src/bookings/BookingService.java
+++ src/bookings/BookingService.java
@@ -1,17 +1,16 @@
 package bookings;
 
-import cars.Car;
-import cars.CarDAO;
-import users.User;
-
 import static enums.Enum.VEHICULE_TYPE.ELECTRIC;
+
+import cars.Car;
+import cars.CarArrayDataAccessService;
+import users.User;
 
 public class BookingService {
 
     private static Book booking;
     private static final int CAPACITY = 10;
     private static int nextIteration = 0;
-
 
     public BookingService(Book booking) {
         BookingService.booking = booking;
@@ -21,25 +20,25 @@ public class BookingService {
         booking.put(car, user);
     }
 
-    public void getAllUsersBookCars() {
-        for (Car car : CarDAO.getCars()) {
+    public void getAllUsersBookCars(CarArrayDataAccessService carDAS) {
+        for (Car car : carDAS.getCars()) {
             if (booking.getBooking().containsKey(car)) {
                 System.out.println(car);
             }
         }
     }
 
-    public void getAvailableCars() {
-        for (Car car : CarDAO.getCars()) {
+    public void getAvailableCars(CarArrayDataAccessService carDAS) {
+        for (Car car : carDAS.getCars()) {
             if (!booking.getBooking().containsKey(car)) {
                 System.out.println(car);
             }
         }
     }
 
-    public static Car[] getAvailableElectricCars() {
+    public static Car[] getAvailableElectricCars(CarArrayDataAccessService carDAS) {
         Car[] availableElectricCars = new Car[CAPACITY];
-        for (Car car : CarDAO.getCars()) {
+        for (Car car : carDAS.getCars()) {
             try {
                 if (!booking.getBooking().containsKey(car) && car.getVehiculeType().equals(ELECTRIC)) {
                     availableElectricCars[nextIteration++] = car;
