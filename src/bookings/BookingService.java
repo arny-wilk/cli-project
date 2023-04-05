433fa42ed27384269a42a65e1fde07a9d1754bdc
arny-wilk <arnywilk@gmail.com>
2023-04-05 23:39:16 +0200

    .gitignore modified

--- src/bookings/BookingService.java
+++ src/bookings/BookingService.java
@@ -2,8 +2,6 @@ package bookings;
 
 import static enums.Enum.VEHICULE_TYPE.ELECTRIC;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Optional;
 import java.util.Scanner;
 import java.util.UUID;
